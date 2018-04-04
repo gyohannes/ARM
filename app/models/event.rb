e24e4b106714697791a94fa6bdae98b24942b919
G/yohannes Zenebe <gyohannes1@gmail.com>
2018-04-04 11:04:59 +0300

    update

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,5 +2,12 @@ class Event < ApplicationRecord
   belongs_to :academic_year
 
   validates :academic_year_id, :name, :start_time, :end_time, presence: true
+  validate :end_time_must_be_after_start_time
+
+  def end_time_must_be_after_start_time
+    if end_time < start_time
+      errors.add(:end_time, 'must be after start time')
+    end
+  end
 
 end
