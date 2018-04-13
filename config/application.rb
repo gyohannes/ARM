eed757a232bd4a8989eeb4cd540102f6f8b1c6f5
G/yohannes Zenebe <gyohannes1@gmail.com>
2018-04-13 15:45:14 +0300

    update

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'ethiopic_date'
-include EthiopicDate
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
