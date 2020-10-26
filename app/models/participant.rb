640e8d83cd367c7db855b10c4ff089dac32b57cc
G/yohannes Zenebe <gyohannes1@gmail.com>
2020-10-26 15:50:39 +0300

    Updating ID Card

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -22,9 +22,21 @@ class Participant < ApplicationRecord
     event = Event.current
     CSV.foreach(file.path, :headers=>true, encoding: 'iso-8859-1:utf-8') do |row|
       name = row[0]
-      organization_type = row[1].blank? ? nil : OrganizationType.find_or_create_by(name: row[1])
-      telephone_number = row[2]
-      attrbts = {event_id: event.try(:id), name: name, organization_type_id: organization_type.try(:id), telephone_number: telephone_number}
+      region = row[1].blank? ? nil : Region.find_or_create_by(name: row[1])
+      organization_type = row[2].blank? ? nil : OrganizationType.find_or_create_by(name: row[2])
+      organization = row[3]
+      position = row[4]
+      address = row[5]
+      telephone_number = row[6]
+      email = row[7]
+      participant_type = row[8].blank? ? nil : ParticipantType.find_or_create_by(name: row[8])
+      stay_at = row[9]
+      group = row[10].blank? ? nil : Group.find_or_create_by(name: row[10])
+      field_visit = row[11].blank? ? nil : FieldVisit.find_or_create_by(name: row[11])
+      attrbts = {event_id: event.try(:id), name: name, region_id: region.try(:id), organization: organization,
+                 organization_type_id: organization_type.try(:id), position: position, address: address,
+                 telephone_number: telephone_number, email: email, participant_type_id: participant_type.try(:id),
+                 stay_at: stay_at, group_id: group.try(:id), field_visit_id: field_visit.try(:id)}
       particpant = Participant.create(attrbts)
       participants << particpant unless particpant.blank?
     end
@@ -33,13 +45,10 @@ class Participant < ApplicationRecord
 
   def set_serial_number
     serial_no = (Participant.count + 1).to_s
-    while serial_no.length < 4
+    while serial_no.length < 3
       serial_no =  '0' << serial_no
     end
     self[:serial_number] = serial_no
   end
 
-  def checkedin_status
-    checked_in == true ? 'Yes' : ''
-  end
 end
