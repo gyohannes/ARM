d07455055450739143cf210f3681e41ede1cf4cc
G/yohannes Zenebe <gyohannes1@gmail.com>
2021-09-14 09:26:21 +0300

    update

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -8,7 +8,7 @@ class Participant < ApplicationRecord
   belongs_to :user, optional: true
   validates :name, :organization, :email, :email_confirmation, :address, :telephone_number, presence: true
   #validates :serial_number, uniqueness: { case_sensitive: false }
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  #validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :email, confirmation: true
 
   has_one_attached :photo
