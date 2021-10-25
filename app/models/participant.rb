d420ae3c158a695d2261da19b4a1713ba28ad656
G/yohannes Zenebe <gyohannes1@gmail.com>
2021-10-25 12:26:01 +0300

    Update

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,7 @@
 class Participant < ApplicationRecord
   belongs_to :event
-  belongs_to :organization_type
+  belongs_to :organization_type, optional: true
+  belongs_to :directorate, optional: true
   belongs_to :group, optional: true
   belongs_to :field_visit, optional: true
   belongs_to :participant_type
@@ -23,18 +24,17 @@ class Participant < ApplicationRecord
     CSV.foreach(file.path, :headers=>true, encoding: 'iso-8859-1:utf-8') do |row|
       name = row[0]
       region = row[1].blank? ? nil : Region.find_or_create_by(name: row[1])
-      organization_type = row[2].blank? ? nil : OrganizationType.find_or_create_by(name: row[2])
-      organization = row[3]
-      position = row[4]
-      address = row[5]
+      organization = row[2]
+      position = row[3]
+      directorate = row[4].blank? ? nil : Directorate.find_or_create_by(name: row[4])
+      email = row[5]
       telephone_number = row[6]
-      email = row[7]
-      participant_type = row[8].blank? ? nil : ParticipantType.find_or_create_by(name: row[8])
-      stay_at = row[9]
-      group = row[10].blank? ? nil : Group.find_or_create_by(name: row[10])
-      field_visit = row[11].blank? ? nil : FieldVisit.find_or_create_by(name: row[11])
+      participant_type = row[7].blank? ? nil : ParticipantType.find_or_create_by(name: row[7])
+      stay_at = row[8]
+      group = row[9].blank? ? nil : Group.find_or_create_by(name: row[9])
+      field_visit = row[10].blank? ? nil : FieldVisit.find_or_create_by(name: row[10])
       attrbts = {event_id: event.try(:id), name: name, region_id: region.try(:id), organization: organization,
-                 organization_type_id: organization_type.try(:id), position: position, address: address,
+                 position: position, directorate: directorate.try(:id),
                  telephone_number: telephone_number, email: email, participant_type_id: participant_type.try(:id),
                  stay_at: stay_at, group_id: group.try(:id), field_visit_id: field_visit.try(:id)}
       particpant = Participant.create(attrbts)
