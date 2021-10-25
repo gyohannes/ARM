5d39c0c4a6e1782e0e51ef322c5230df733c2cb6
G/yohannes Zenebe <gyohannes1@gmail.com>
2021-10-25 13:58:51 +0300

    update

--- db/migrate/20211025104554_add_directorate_to_participant.rb
+++ db/migrate/20211025104554_add_directorate_to_participant.rb
@@ -1,5 +1,5 @@
 class AddDirectorateToParticipant < ActiveRecord::Migration[5.2]
   def change
-    add_reference :directorates, type: :uuid
+    add_reference(:participants, :directorate, type: :uuid, foreign_key: true)
   end
 end
